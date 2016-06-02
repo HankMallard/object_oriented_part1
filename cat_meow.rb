d9d2d6113a7de8f7d1011572a51d0ca371d285b2
howardl01@live.ca <howardl01@live.ca>
2016-06-02 15:10:04 -0400

    cat meal time

--- cat_meow.rb
+++ cat_meow.rb
@@ -1,10 +1,23 @@
 class Cat
 
   attr_accessor :name, :preferred_food, :meal_time
-
+# general attribute of Cats
   def initialize(name, preferred_food, meal_time)
     @name=name
     @preferred_food=preferred_food
     @meal_time=meal_time
   end
-end  
+# statement of cats meal_time
+  def eat_at
+    if @meal_time < 12
+      puts "#{@name} eats at#{@meal_time}AM"
+    else
+      puts "#{@name} eat at #{@meal_time}PM"
+    end
+  end
+end
+
+# Two new instance of cats
+spot=Cat.new('spot','tuna',10)
+
+mal=Cat.new('mal', 'bass', 14)
